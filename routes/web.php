6793b2cf0eefdc8fa6d764ef777d0a5b1d5abda8
zakaria <zikokh012@gmail.com>
2024-02-14 16:57:13 +0100

    commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
+Route::get('/register', [\App\Http\Controllers\AuthController::class, 'register'])->name('register');
